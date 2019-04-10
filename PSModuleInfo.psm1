15907cf7fa1bea7a68ed75c1f9df63078ccd3599
LxLeChat <43340127+LxLeChat@users.noreply.github.com>
2019-04-10 11:37:25 +0200

    Update PSModuleInfo.psm1
    changes route, now using packages() instead of search().
    did some other stuff...

--- PSModuleInfo.psm1
+++ PSModuleInfo.psm1
@@ -17,24 +17,7 @@ function Get-PSModuleInfo {
         Dependencies             :
         Description              : Contains a set of utilities to work with Powershell Classes.
         DownloadCount            : DownloadCount
-        GalleryDetailsUrl        : https://www.powershellgallery.com/packages/PSClassUtils/0.10.0
-        IconUrl                  : IconUrl
-        IsLatestVersion          : IsLatestVersion
-        IsAbsoluteLatestVersion  : IsAbsoluteLatestVersion
-        IsPrerelease             : IsPrerelease
-        Language                 : Language
-        LastUpdated              : LastUpdated
-        Published                : Published
-        PackageHash              : gENnbohfT7my02tp4IFsvw1qKQDzX1NJWB4lOa1+AtCLmrQZBcn86xFtYJQcQD7drV9jRXIiY3QZ6yIPVZyeBA==
-        PackageHashAlgorithm     : SHA512
-        PackageSize              : PackageSize
-        ProjectUrl               : ProjectUrl
-        ReportAbuseUrl           : https://www.powershellgallery.com/packages/PSClassUtils/0.10.0/ReportAbuse
-        ReleaseNotes             : ReleaseNotes
-        RequireLicenseAcceptance : RequireLicenseAcceptance
-        Summary                  : Summary
-        Tags                     : PSModule
-        Title                    : Title
+        ...
         VersionDownloadCount     : VersionDownloadCount
     .INPUTS
         Module(s) Names, or partial Module name
@@ -42,7 +25,7 @@ function Get-PSModuleInfo {
     .OUTPUTS
         Module(s) Info(s) from the PSGallery
     .NOTES
-        General notes
+        Go checke: https://github.com/LxLeChat/PSModuleInfo
     #>
     [CmdletBinding()]
     Param (
@@ -75,7 +58,6 @@ function Get-PSModuleInfo {
                 
             }
 
-
             If ( $M -match '\*$') {
 
                 If ( $LatestVersion ) {
@@ -100,23 +82,29 @@ function Get-PSModuleInfo {
     
     End {
 
+        ## Finish query
         If ( $LatestVersion ) {
             $Q = $Q + ')'
         }
 
         $fQ = $bQ + $Q
-        $Uri = "https://www.powershellgallery.com/api/v2/Search()?$fQ&`$orderby=Id"
-
-        Write-Verbose $Uri
+        $Uri = "https://www.powershellgallery.com/api/v2/Packages()?$fQ&`$orderby=Id"
 
         If ( $DownLoadCount ) {
-            $ApiCall=Invoke-RestMethod -Method GET -Uri $Uri | Select-Object -ExpandProperty properties
+
+            ## API Call
+            $ApiCall = Invoke-RestMethod -Method GET -Uri $Uri | Select-Object -ExpandProperty properties
+
+            ## If downloadcount switch is used, we need to fetch all properties, remove downloadcount, and versiondownloadcount, and readd them as calculated properties
             [System.Collections.ArrayList]$Properties = ($ApiCall | Get-Member -MemberType Property).Name
             $Properties.Remove('VersionDownloadCount')
             $Properties.Remove('DownloadCount')
             $null = $Properties.Add(@{l='DownLoadCount';e={$_.DownLoadCount.'#Text'}})
             $null = $Properties.Add(@{l='VersionDownLoadCount';e={$_.VersionDownLoadCount.'#Text'}})
             
+            ## Return object with reformated downloadcount and versiondownloadcount
+            $ApiCall | select-object -Property $Properties -ExcludeProperty DownloadCount,VersionDownloadCount
+
             ## Results might be paginated, we need yo generate the next url
             If ( $ApiCall.count -eq 100 ) {
                 $skip = 100
@@ -126,24 +114,27 @@ function Get-PSModuleInfo {
                     } Else {
                         $Uri = $uri -replace '\$skip=\d+$',"`$skip=$skip"
                     }
-                    write-verbose "Next Url: $uri"
-                    $ApiCall=Invoke-RestMethod -Method GET -Uri $Uri | Select-Object -ExpandProperty properties
-                    [System.Collections.ArrayList]$Properties = ($ApiCall | Get-Member -MemberType Property).Name
-                    $Properties.Remove('VersionDownloadCount')
-                    $Properties.Remove('DownloadCount')
-                    $null = $Properties.Add(@{l='DownLoadCount';e={$_.DownLoadCount.'#Text'}})
-                    $null = $Properties.Add(@{l='VersionDownLoadCount';e={$_.VersionDownLoadCount.'#Text'}})
+
+                    ## API Call
+                    $ApiCall = Invoke-RestMethod -Method GET -Uri $Uri | Select-Object -ExpandProperty properties
+
+                    ## Return object with reformated downloadcount and versiondownloadcount
                     $ApiCall | select-object -Property $Properties -ExcludeProperty DownloadCount,VersionDownloadCount
                     $skip = $skip + 100
                 }
-            } Else {
-                $ApiCall | select-object -Property $Properties -ExcludeProperty DownloadCount,VersionDownloadCount
             }
+
         } Else {
+
+            ## API Call
             $ApiCall = Invoke-RestMethod -Method GET -Uri $Uri | Select-Object -ExpandProperty properties
+
+            ## Return raw object
+            $ApiCall
 
             ## Results might be paginated, we need yo generate the next url
             if ( $ApiCall.count -eq 100 ) {
+                ## Return raw object
                 $ApiCall
                 $skip = 100
                 While ( $ApiCall.count -eq 100 ) {
@@ -152,14 +143,13 @@ function Get-PSModuleInfo {
                     } Else {
                         $Uri = $uri -replace '\$skip=\d+$',"`$skip=$skip"
                     }
-                    
-                    write-verbose "Next Url: $uri"
+
+                    ## API Call
                     $ApiCall = Invoke-RestMethod -Method GET -Uri $Uri | Select-Object -ExpandProperty properties
+                    ## Return raw object
                     $ApiCall
                     $skip = $skip + 100
                 }
-            } Else {
-                $ApiCall
             }
         }
     }
